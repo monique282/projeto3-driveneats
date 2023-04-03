4088165f721c433c9bd44926d96f2edf22860adb
monique <moniquejp16@gmail.com>
2023-04-03 16:18:36 -0300

    finalizado

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 let nomePrato = '';
 let nomeBebida = '';
 let nomesobremesa = '';
-let x, x1, y, y1, z, z1 , m = '';
+let x, x1, y, y1, z, z1, m = '';
 
 let total = '';
 
 function selecionarTudo() {
+
     // verificar se o pratos foi clicado
     if (nomePrato !== '') {
+
         // verificar se bebida foi clicado
         if (nomeBebida !== '') {
 
             // verificar se sobremesa foi clicado
             if (nomesobremesa !== '') {
+
                 // buscar a class caixa-finalizar
                 const caixaFinalizar = document.querySelector('.caixa-finalizar');
+
                 // adiciona a clss botão-finalizar
                 caixaFinalizar.classList.add('botão-finalizar');
                 caixaFinalizar.innerHTML = 'Fechar Pedido';
                 document.getElementsByClassName("caixa-finalizar").disabled = true;
-
                 let btnRemove = document.getElementById('delete').removeAttribute('disabled');
             }
         }
@@ -29,17 +32,15 @@ function selecionarTudo() {
 
 function valorTotal(terminando) {
 
-
     // verificar se existe a class aparecer
     const confirmar = document.querySelector('.tudo .aparecer');
+
     // se exixtir ele retira
     if (confirmar !== null) {
         confirmar.classList.remove('aparecer');
     }
-
     total = Number(x1) + Number(y1) + Number(z1);
     m = parseFloat(total).toFixed(2);
-    console.log(m);
 
     const mudar = document.querySelector('.valor-t');
     mudar.innerHTML = `${m}`;
@@ -55,68 +56,49 @@ function valorTotal(terminando) {
 
     const mudarPra = document.querySelector('.prato-selec p');
     mudarPra.innerHTML = `${nomePrato}`;
-    console.log(nomePrato);
 
     const mudarBeb = document.querySelector('.bebida-selec p');
     mudarBeb.innerHTML = `${nomeBebida}`;
-    console.log(nomeBebida);
 
     const mudarSob = document.querySelector('.sobremesa-selec p');
     mudarSob.innerHTML = `${nomesobremesa}`;
-    console.log(nomesobremesa);
-
-
 }
 
 function selecionarPrato(qualPrato) {
 
-
     // verificar se existe um dos pratos ja com borda
     // vendo se tem a class borda nele
     const pratoClicadoAntes = document.querySelector('.pratos .borda');
-    const vezinhov = document.querySelector('.preço-prato .vezinho');
-    console.log(vezinhov);
 
     // se ja existe um prato com borda 
     if (pratoClicadoAntes !== null) {
+
         //remover a borda
         pratoClicadoAntes.classList.remove('borda');
-    
-        
     }
     // adicionar a borda a ele
     qualPrato.classList.add('borda');
-    
-    
-    
 
     // guardar nome
     const nomeP = document.querySelector('.borda .nome')
     nomePrato = nomeP.innerHTML;
-    console.log(nomePrato);
 
     // guardar valor
     x = document.querySelector('.borda p');
-
     x1 = x.innerHTML.replace('R$', '').replace(',', '.');
 
-    console.log(Number(x1));
-
     selecionarTudo();
-
 }
 
 
-
 function selecionarBebida(qualBebida) {
-
-    nomeBebida = qualBebida.innerHTML;
-
     // verificar se existe um dos pratos ja com borda
     // vendo se tem a class borda nele
     const bebidaClicadoAntes = document.querySelector('.bebidas .borda');
+
     // se ja existe um prato com borda 
     if (bebidaClicadoAntes !== null) {
+
         //remover a borda
         bebidaClicadoAntes.classList.remove('borda');
     }
@@ -127,26 +109,20 @@ function selecionarBebida(qualBebida) {
     // guardar nome
     const nomeB = document.querySelector('.borda .nome-b')
     nomeBebida = nomeB.innerHTML;
-    console.log(nomeBebida);
 
     // guardar valor
-
     y = document.querySelector('.borda .preço p');
-
     y1 = y.innerHTML.replace('R$', '').replace(',', '.');
-
-    console.log(Number(y1));
 
     selecionarTudo();
 }
 
 function selecionarSobremesa(qualSobremesa) {
 
-    nomesobremesa = qualSobremesa.innerHTML;
-
     // verificar se existe um dos pratos ja com borda
     // vendo se tem a class borda nele
     const sobremesaClicadoAntes = document.querySelector('.sobremesa .borda');
+
     // se ja existe um prato com borda 
     if (sobremesaClicadoAntes !== null) {
         //remover a borda
@@ -159,12 +135,9 @@ function selecionarSobremesa(qualSobremesa) {
     // guardar nome
     const nomeS = document.querySelector('.borda .nome-s')
     nomesobremesa = nomeS.innerHTML;
-    console.log(nomesobremesa);
 
     // guardar valor
-
     z = document.querySelector('.borda .preço-sobremesa p');
-
     z1 = z.innerHTML.replace('R$', '').replace(',', '.');
 
     console.log(Number(z1));
@@ -174,15 +147,16 @@ function selecionarSobremesa(qualSobremesa) {
 
 function pedir() {
 
-    const mensagem = `Olá, gostaria de fazer o pedido\n- 
-    Prato: ${nomePrato}\n-
-     Bebida: ${nomeBebida}\n- 
-     Sobremesa: ${nomesobremesa}\n
-     Total: R$ ${m}`
+    const mensagem = `Olá, gostaria de fazer o pedido
+    Prato:${nomePrato}
+    Bebida:${nomeBebida}
+    Sobremesa:${nomesobremesa}
+    Total: R$ ${m}`
 
+    alert(mensagem);
     const encode = encodeURIComponent(mensagem);
     window.open(`https://wa.me/38998152601?text=${encode}`);
-
+ 
 }
 
 function cancelar(cancel) {
